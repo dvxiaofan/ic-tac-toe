67b3069fd57af744dd881131571972316a10c380
xiaofan <1353815633@qq.com>
2018-06-29 16:04:22 +0800

    状态提升， 通过父控件控制子控件的数据更新

--- src/index.js
+++ src/index.js
@@ -4,26 +4,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	constructor() {
-		// 必须调用 super() 方法才能在继承父类的子类中正确获取到类型的 this 
-		super();
-		this.state = {
-			value: null
-		};
-	}
 
   render() {
     return (
-      <button className="square" onClick={ () => this.setState({ value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={ () => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			// 创建空数组存放数据
+			squares: Array(9).fill(null)
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({ squares: squares });
+	}
+
   renderSquare(i) {
-    return <Square value={i} />;
+		return (
+			<Square 
+				// value 属性中传递对应 state 数组元素的值
+				value={this.state.squares[i]} 
+				// 传递一个事件处理函数到 Square 当中
+				onClick={() => this.handleClick(i)}
+			/>
+		);
   }
 
   render() {
